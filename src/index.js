4275e3bdbdfea6758bc25f2b8eaae54aa850e703
Yasir Khan <khanyaser007@gmail.com>
2022-10-25 14:40:14 +0500

    Add search bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-import './style.css';
+import './style.scss';
+// import addElem from './modules/add-elem.js';
+
+// Search button
+const searchIcon = document.querySelector('#search-btn');
+const searchBarContainer = document.querySelector('.search-bar');
+const searchCloseBtn = document.querySelector('#search-close-btn');
+const menuIcon = document.querySelector('#menu-icon');
+const header = document.querySelector('header');
+
+searchIcon.onclick = () => {
+  searchBarContainer.classList.remove('hide');
+
+  // Add event listener
+  searchCloseBtn.onclick = () => {
+    searchBarContainer.classList.add('hide');
+  };
+};
+
+// Search Bar For Desktop
+if (window.innerWidth > 768) {
+  searchBarContainer.classList.remove('hide');
+  const menuText = document.createElement('span');
+  menuText.textContent += 'Menu';
+  menuIcon.append(menuText);
+  searchCloseBtn.classList.add('hide');
+  searchIcon.classList.add('hide');
+
+  // Add Search Icon
+  const searchIconDesktopBtn = document.createElement('button');
+  searchIconDesktopBtn.setAttribute('type', 'button');
+  searchIconDesktopBtn.classList.add('searchIconDesktopBtn');
+  const searchIconDesktopSpan = document.createElement('span');
+  searchIconDesktopSpan.classList.add('material-icons-round', 'icons');
+  searchIconDesktopSpan.textContent = 'search';
+
+  searchIconDesktopBtn.append(searchIconDesktopSpan);
+  header.append(searchIconDesktopBtn);
+}
